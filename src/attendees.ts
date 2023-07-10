731ff0ab12d7e7e335b847782a102607b3d32acb
Wilhelm <wmauch@revival.com>
2023-07-09 21:32:54 -0400

    Delete attendees.ts

--- src/attendees.ts
+++
@@ -1,5 +0,0 @@
-import { Attendee } from './Types'
-
-export const attendees: Attendee[] = [
-    {first : "Wilhelm", last : "Mustermann", email: 'rutech@revival.com', phone : '8134456232', barcode : '126634', fam : false}
-]
